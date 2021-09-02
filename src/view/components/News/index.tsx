ab808137333983066a2f9f8ac61ff7211637c949
Имя Фамилия <mihay0113@gmail.com>
2021-09-02 11:17:25 +0300

    first react lesson

---
+++ src/view/components/News/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Article } from '../Article';
+import { Section } from './styles';
+
+export const News = () => {
+    return (
+        <Section>
+            <Article></Article>
+            <Article></Article>
+            <Article></Article>
+        </Section>
+    );
+};
